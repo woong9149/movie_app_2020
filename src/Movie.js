bdad748ddf91a727b1ab91a3619c78709db7bd23
woong9149 <sewoong0409@naver.com>
2020-07-24 22:02:40 +0900

    Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 
 function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>;
+    return <div class="movie">
+                <img src={poster} alt={title} title={title}/>
+                <div class="movie__data">
+                    <h3 class="movie__title">{title}</h3>
+                    <h5 class="movie__year">{year}</h5>
+                    <p class="movie__summary">{summary}</p>
+                </div>
+            </div>;
 }
 
 Movie.propTypes = {//왜 p로 하면 잘되고 P로 하면 오류나는지 ?
